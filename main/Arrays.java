edee121196ce850494359cb85c0f3b93a718199e
obarolli <obarolli@gmail.com>
2021-06-07 15:08:27 -0700

    Removed unnecessary redundancy

--- main/Arrays.java
+++ main/Arrays.java
@@ -1,5 +1,7 @@
 package main;
 
+import utils.HomeworkAssignment;
+
 public class Arrays extends HomeworkAssignment {
 	
 	public static void main(String[] args) {
@@ -91,42 +93,4 @@ public class Arrays extends HomeworkAssignment {
 		
 		printOut(java.util.Arrays.toString(arr));
 	}
-}
-
-
-
-abstract class HomeworkAssignment {
-	
-	private String[] questions;
-	
-	public void initQuestionsList(int length) {
-		questions = new String[length];
-
-		for (int i = 0; i < length; ++i) {
-			questions[i] = "question" + (i + 1);
-		}
-	}
-	
-	public void runAllQuestions() {
-		
-		try {
-
-			for (int i = 0; i < questions.length; ++i) {
-				printOut("Question " + (i + 1));
-				this.getClass().getMethod(questions[i]).invoke(this);
-				printOut("-------------------------------------");
-			}
-		}
-		catch (Exception e) {
-			printOut("Uh oh! Exception!");
-			printOut(e.getMessage());
-		}
-		
-	}
-
-	public void printOut(Object s) {
-		System.out.println(s);
-	}
-
-	public abstract void run();
 }
